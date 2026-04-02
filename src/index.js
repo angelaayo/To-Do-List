1e7b5f61ec606329b6735c7c2733284fcb80f757
Angela <angelaayodeji@gmail.com>
2026-04-02 13:18:02 -0500

    quick function declaration for modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "./style.css";
 import {task} from "./task.js";
 console.log("Hello world");
+
+function init(){
+    document.querySelector(".addBtn").addEventListener("click", handleTaskAdd);
+
+    function handleTaskAdd(){
+        document.querySelector("#popUp").classList.add("show");
+
+    }
+}
+init();
